1e15699d27187fd460991e30e85f2ed270981f74
hakimhassani97 <hakimhassani97@gmail.com>
2020-03-21 14:16:04 +0100

    added cdn for p5js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert()
+let system;
+window.addEventListener('resize',(e)=>{
+    createCanvas(window.innerWidth,window.innerHeight-4)
+})
+function setup() {
+    // createCanvas(720, 400);
+    createCanvas(window.innerWidth,window.innerHeight-4)
+    system = new ParticleSystem(createVector(width / 2, 50))
+}
+
+function draw() {
+    background(51);
+    system.addParticle();
+    system.run();
+}
+function mouseClicked(){
+    system = new ParticleSystem(createVector(mouseX, mouseY))
+}
